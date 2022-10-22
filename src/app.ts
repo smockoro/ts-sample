3e0f47ad89ce3307c2a18a17ba0f982654cedbf5
takahiro oshima <t-oshima@nri.co.jp>
2022-10-22 20:39:48 +0900

    refactor(index.ts->app.ts,server.ts): supertestのためにindex.tsを分割

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express, { Express, Request, Response } from 'express'
 
 const app: Express = express()
-const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world')
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+export default app
