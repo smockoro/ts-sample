3e211836bc0dd9c2baad5894cce96d14e10c1076
takahiro oshima <t-oshima@nri.co.jp>
2022-10-22 21:04:27 +0900

    test(hello.test.ts): とりあえずhelloworldを返却するAPIの自動テストサンプルを追加
    ついでにJestの設定も追加。

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Express, Request, Response } from 'express'
 const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world')
+  res.send({message: "Hello world!"})
 })
 
 export default app
