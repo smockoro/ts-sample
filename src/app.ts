918a98ec6938587c8018f64797bbe43b6000b057
takahiro oshima <t-oshima@nri.co.jp>
2022-10-24 02:05:58 +0900

    test(metrics.test.ts): /metricsのチェックテスト追加

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,22 @@ import express, { Express, Request, Response } from 'express'
 import logger from './logging/logger';
 import accessLog from './http/middleware/accessLog';
 import compressFilter from './http/middleware/compress';
-import prometheusClient from 'prom-client';
+import {Registry, collectDefaultMetrics} from 'prom-client';
 
-const register = new prometheusClient.Registry()
-prometheusClient.collectDefaultMetrics({register})
+const register: Registry = new Registry()
+collectDefaultMetrics({register})
 const app: Express = express()
 
-app.use(accessLog)
-app.use(compressFilter)
-
-app.get("/metrics", async (req, res) => {
+// アクセスログフィルタに引っかから内容に手前に持っていく。
+// readiness/livenessもここと同様にする。
+app.get("/metrics", async (req: Request, res: Response) => {
   res.set("Content-Type", register.contentType);
   let metrics = await register.metrics();
   res.send(metrics);
 });
+
+app.use(accessLog)
+app.use(compressFilter)
 
 app.get('/', (req: Request, res: Response) => {
   logger.info("hello log message!!")
