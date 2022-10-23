9309acfd2229bbe2e81b2e00207062a1afc0ef11
takahiro oshima <t-oshima@nri.co.jp>
2022-10-24 01:30:27 +0900

    feat(prom-client): /metricsでprometheusのメトリクスを出力するようにした

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import express, { Express, Request, Response } from 'express'
 import logger from './logging/logger';
 import accessLog from './http/middleware/accessLog';
 import compressFilter from './http/middleware/compress';
+import prometheusClient from 'prom-client';
 
+const register = new prometheusClient.Registry()
+prometheusClient.collectDefaultMetrics({register})
 const app: Express = express()
 
 app.use(accessLog)
 app.use(compressFilter)
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  let metrics = await register.metrics();
+  res.send(metrics);
+});
 
 app.get('/', (req: Request, res: Response) => {
   logger.info("hello log message!!")
