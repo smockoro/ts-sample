8f2059ba16d03be629d6666bb4bb4aa929f3c6f0
takahiro oshima <tarotora51@gmail.com>
2022-11-05 21:28:26 +0900

    feat(error handler): エラーハンドリングとエラー型

--- fastify-docker/src/app.ts
+++ fastify-docker/src/app.ts
@@ -1,11 +1,25 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import {pingPath, pingOpts, pingController } from './adapter/http/route/ping'
 import { loggerConfig } from "./logging/logger";
+import { SystemError } from "./core/error/error";
 
 export default function buildFastify() {
   const fastify: FastifyInstance = Fastify({logger: loggerConfig})
 
   fastify.get(pingPath, pingOpts, pingController)
+
+  fastify.setErrorHandler(function (error: Error, request, reply) {
+    fastify.log.error(error)
+    if (error instanceof SystemError) {
+      fastify.log.error(`${error.code}, ${error.message}`)
+    }
+    reply.status(500).send({'message': "unexpected error occur."})
+  })
+  fastify.addHook('onError', async (req, rep, error) => {
+    fastify.log.error("onError Section")
+    fastify.log.error(error)
+    rep.header("X-Custom-Header", "onError")
+  })
 
   return fastify
 }
--- fastify-docker/src/server.ts
+++ fastify-docker/src/server.ts
@@ -4,7 +4,10 @@ const fastify = buildFastify()
 
 const start = async () => {
   try {
-    await fastify.listen({port: 3000})
+    await fastify.listen({port: 3000}, () => {
+      fastify.log.debug(fastify.printPlugins())
+      fastify.log.debug(fastify.printRoutes())
+    })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
