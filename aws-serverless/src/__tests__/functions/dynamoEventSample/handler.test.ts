8ee6d32eb8461e79817cf138a18b2fac3237560c
smockoro <tarotora51@gmail.com>
2023-01-18 03:24:45 +0900

    chore(テストコードが腐ったので一旦元に戻す。):

--- aws-serverless/src/__tests__/functions/dynamoEventSample/handler.test.ts
+++ aws-serverless/src/__tests__/functions/dynamoEventSample/handler.test.ts
@@ -1,14 +1,17 @@
-import { debugDynamoDB } from '../../../functions/debugDynamo/handler';
+import { DynamoDBStreamEvent } from 'aws-lambda';
+import { dynamoEventSample } from '../../../functions/dynamoEventSample/handler';
 import { dummyContext } from '../__utils__/util';
 
 describe('handler test', (): void => {
   test('sample test1', async () => {
-    const e =  {
-      body: {
-        name: "alice",
-        email: "a.user@example.com",
-      },
+    const e: DynamoDBStreamEvent =  {
+      Records: [
+        {
+          awsRegion: 'ap-northeast-1',
+          eventName: 'INSERT',
+        },
+      ],
     }
-    debugDynamoDB(e, dummyContext)
+    dynamoEventSample(e, dummyContext)
   });
 });
