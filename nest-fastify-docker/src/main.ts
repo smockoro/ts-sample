61672204c4b5f0eb4dc9cd17b8c841915782b5b9
smockoro <tarotora51@gmail.com>
2023-04-09 04:20:58 +0900

    feat(nest-fastify-docker): CORS対応

--- nest-fastify-docker/src/main.ts
+++ nest-fastify-docker/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-    { bufferLogs: true },
+    { bufferLogs: true, cors: true },
   );
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
