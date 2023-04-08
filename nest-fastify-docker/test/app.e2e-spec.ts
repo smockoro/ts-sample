da209f08600adad7dc2e998841c6c273092cb273
smockoro <tarotora51@gmail.com>
2023-04-08 13:15:36 +0900

    feat(nest-fastify-docker): expressからfastifyに対応した形に変更

--- nest-fastify-docker/test/app.e2e-spec.ts
+++ nest-fastify-docker/test/app.e2e-spec.ts
@@ -1,24 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return app.inject({ url: '/' }).then(() => expect('Hello World!'));
   });
 });
