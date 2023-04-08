16ca09cb4bdcb933c5b36f93866bee28389a43c4
smockoro <tarotora51@gmail.com>
2023-04-08 15:55:49 +0900

    feat(nest-fastify-docker): winstonでロギングするようにした

--- nest-fastify-docker/src/app.module.ts
+++ nest-fastify-docker/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+import * as process from 'process';
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format:
+            process.env.NODE_ENV !== 'production'
+              ? winston.format.combine(
+                  winston.format.timestamp(),
+                  nestWinstonModuleUtilities.format.nestLike('SampleApp', {
+                    colors: true,
+                    prettyPrint: true,
+                  }),
+                )
+              : winston.format.combine(winston.format.timestamp()),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-fastify-docker/src/main.ts
+++ nest-fastify-docker/src/main.ts
@@ -4,12 +4,15 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
